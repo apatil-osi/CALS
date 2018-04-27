29069f65e5fb33b7cf0b3661990046229bb6383c
Nasar-cambria <nkprodware@gmail.com>
2018-04-27 09:38:42 -0700

    changing dropdown select to radio

--- app/javascript/rfa_forms/rfa01a_edit_view/minorCardField.js
+++ app/javascript/rfa_forms/rfa01a_edit_view/minorCardField.js
@@ -92,46 +92,20 @@ export class MinorCardField extends React.Component {
           value={getDictionaryId(minor.gender)}
           label={isRelationShipToApplicantObject ? 'Gender (required)' : 'Gender'}
           onChange={(event) => this.props.onFieldChange(this.props.index, dictionaryNilSelect(event.target.options), 'gender')} />
-        <div>
-          <YesNoRadioComponent
-            label={isRelationShipToApplicantObject ? 'Do you financially support this child? (required)' : 'Do you financially support this child?'}
-            idPrefix='child_financially_supported'
-            value={minor.child_financially_supported}
-            selectClassName={'reusable-select'}
-            optionList={yesNo.items}
-            onFieldChange={(event) => this.props.setParentState('child_financially_supported', event.target.value)} />
-        </div>
-
-         <div>
-          <YesNoRadioComponent
-            idPrefix='child_adopted'
-            value={minor.child_adopted}
-            selectClassName={'reusable-select'}
-            optionList={yesNo.items}
-            label={isRelationShipToApplicantObject ? 'Is this child adopted? (required)' : 'Is this child adopted?'}
-          
-            onFieldChange={(event) => this.props.onFieldChange(this.props.index, dictionaryNilSelectValue(event.target.value), 'child_adopted')} />
-         </div>
-
-
-
-
-
-
-        <DropDownField gridClassName='col-md-4'
-          id='child_financially_supported'
+        <YesNoRadioComponent
+          label={isRelationShipToApplicantObject ? 'Do you financially support this child? (required)' : 'Do you financially support this child?'}
+          idPrefix='child_financially_supported'
           value={minor.child_financially_supported}
           selectClassName={'reusable-select'}
           optionList={yesNo.items}
-          label={isRelationShipToApplicantObject ? 'Do you financially support this child? (required)' : 'Do you financially support this child?'}
-          onChange={(event) => this.props.onFieldChange(this.props.index, dictionaryNilSelectValue(event.target.options), 'child_financially_supported')} />
-        <DropDownField gridClassName='col-md-4'
-          id='child_adopted'
+          onFieldChange={(event) => this.props.onFieldChange(this.props.index, event.target.value, 'child_financially_supported')} />
+        <YesNoRadioComponent
+          idPrefix='child_adopted'
           value={minor.child_adopted}
           selectClassName={'reusable-select'}
           optionList={yesNo.items}
           label={isRelationShipToApplicantObject ? 'Is this child adopted? (required)' : 'Is this child adopted?'}
-          onChange={(event) => this.props.onFieldChange(this.props.index, dictionaryNilSelectValue(event.target.options), 'child_adopted')} />
+          onFieldChange={(event) => this.props.onFieldChange(this.props.index, event.target.value, 'child_adopted')} />
       </form>
     )
   }
