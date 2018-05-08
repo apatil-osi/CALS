2118bf0369723bcc941a9fcdc095d43ce8fadaa4
Keith Lauher <klauher@cambriasolutions.com>
2018-05-08 11:53:49 -0700

    fix lint round 2

--- app/javascript/components/common/inputFields.js
+++ app/javascript/components/common/inputFields.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const InputComponent = ({gridClassName, fieldClassName, labelClassName,
   id, label, onChange, value, placeholder, type, maxLength, onBlur}) => (
-    <div className={gridClassName}>
-      <label className={labelClassName} htmlFor={id}>{label}</label>
-      <input className={fieldClassName}
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        maxLength={maxLength}
-      />
-    </div>
+  <div className={gridClassName}>
+    <label className={labelClassName} htmlFor={id}>{label}</label>
+    <input className={fieldClassName}
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      maxLength={maxLength}
+    />
+  </div>
 )
 
 export {InputComponent}
