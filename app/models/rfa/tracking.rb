58a2259c02db68c50d1e993d564f0c37452f494d
Keith Lauher <klauher@cambriasolutions.com>
2018-06-05 08:50:03 -0700

    update model

--- app/models/rfa/tracking.rb
+++ app/models/rfa/tracking.rb
@@ -1,7 +1,7 @@
 class Rfa::Tracking < CalsBase
   include Concerns::Rfa::TrackingApiProtocolProvider
 
-attr_accessor :id, :rfa_1a_id, :license_number, :family_name, :family_tracking_documents
+attr_accessor :id, :rfa_1a_id, :license_number, :facility_name, :tracking_documents
 
 def self.parent_path
   'rfa-1a-forms'
